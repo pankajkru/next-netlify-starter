881cd32de0f25dc56943f0a92ef6572d6e888540
Pankaj Kumar <pankajb914@gmail.com>
2026-02-21 10:00:27 +0530

    Update Footer.js
    changing logo

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+    <img src="/logo-nbt.png" alt="Netlify Logo" className={styles.logo} />
         // <a href="https://www.netlify.com" target="_blank" rel="noopener">
         //   <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
         // </a>
