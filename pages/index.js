8dbd500582f2608d77a45b3629e1f009d12d9d33
Pankaj Kumar <pankajb914@gmail.com>
2026-02-21 10:13:05 +0530

    Update index.js
    adding html page

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,40 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>nobrandTrust – Coming Soon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to nobrandTrust!" />
-        <p className="description">
-          Coming Soon!!
+      <main style={{
+        minHeight: '80vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
+      }}>
+        
+        {/* Logo */}
+        <img
+          src="/logo-nbt.png"
+          alt="nobrandTrust Logo"
+          style={{ maxWidth: '220px', marginBottom: '20px' }}
+        />
+
+        {/* Brand Name */}
+        <h1 style={{ color: '#ff8c00', fontSize: '36px', margin: '10px 0' }}>
+          nobrandTrust
+        </h1>
+
+        {/* Coming Soon */}
+        <p style={{ fontSize: '22px', color: '#555' }}>
+          🚧 Coming Soon 🚧
         </p>
+
       </main>
 
       <Footer />
     </div>
   )
 }
+
