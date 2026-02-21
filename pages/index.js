4906b2de373a1c0b53260f4d60c7df989451b7f6
Pankaj Kumar <pankajb914@gmail.com>
2026-02-21 09:58:07 +0530

    Update index.js
    changing to Coming Soon

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to nobrandTrust!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming Soon!!
         </p>
       </main>
 
