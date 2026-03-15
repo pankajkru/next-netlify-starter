1b3de8a283854801eaf39188e45398d4b52636d6
Pankaj Kumar <pankajb914@gmail.com>
2026-03-15 07:48:46 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-    <img src="/logo-nbt.png" alt="Netlify Logo" className={styles.logo} />
+   
         
       </footer>
     </>
